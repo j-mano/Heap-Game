bb7be29da2c77286863ea2e9068c2c0f1f23a799
joachim <joachim@johnsons.nu>
2021-02-20 00:32:18 +0100

    Inital start of the game.
    Inital start of the game. some planing is done and game has been started.

---
+++ Heap/Assets/Scripts/GameScripts/GameStats.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStats : MonoBehaviour
+{
+    int Gold;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Gold = 0;
+    }
+    
+    public int GetGold(){
+        return Gold;
+    }
+
+    public void AddGold(int InputGold){
+        Gold = Gold + InputGold;
+    }
+
+    public void SubtractGold(int InputGold){
+        Gold = Gold + InputGold;
+    }
+}
---
+++ Heap/Assets/Scripts/Interface/Hardware_Printout.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hardware_Printout : MonoBehaviour
+{
+    public Text TitleTxt, Graphics_apiTxt, OSTXT, AplicationRamTxt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        printout();
+    }
+
+    void printout(){
+        Graphics_apiTxt.text     = Get_Gpi_apirunning();
+        OSTXT.text               = GetOs();
+        AplicationRamTxt.text    = GetRamAmountSucking();
+    }
+
+    string GetOs(){
+        return SystemInfo.operatingSystem;
+    }
+
+    string Get_Gpi_apirunning(){
+        return SystemInfo.graphicsDeviceVersion;
+    }
+
+    string GetRamAmountSucking(){
+        return "not yet implemented";
+    }
+}
